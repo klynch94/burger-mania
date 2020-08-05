66d977796db2496baded4fc356a1fcd9e26bafbd
Kieran Lynch <lynch.kieran1@gmail.com>
2020-08-05 16:27:24 -0500

    finalized handlebars by adding a partial

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,42 @@
+$(function() {
+  $(".devour").on("click", function(event) {
+    let id = $(this).data("id");
+    let current = $(this).data("true");
+
+    let burgerEaten = {
+      devoured: current
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: burgerEaten
+    }).then(
+      function() {
+        console.log("changed burger to", current);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+
+    let newBurger = {
+      name: $("#ca").val().trim(),
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
