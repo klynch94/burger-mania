5a2cb8f255795cc4ff23ca8d4d2d4c41ef0d1d2e
Kieran Lynch <lynch.kieran1@gmail.com>
2020-08-05 15:37:34 -0500

    created update route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -26,3 +26,21 @@ router.post("/api/burgers", function (req, res) {
         res.json({ id: result.insertId });
     });
 });
+
+// update burger route
+router.put("/api/burgers/:id", function (req, res) {
+    let condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
