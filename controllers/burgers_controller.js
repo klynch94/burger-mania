ab5069feacb559d344114cdc2dc6f26e4d8998c5
Kieran Lynch <lynch.kieran1@gmail.com>
2020-08-05 15:27:36 -0500

    created show all and post routes in burgers_controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,28 @@
-var express = require("express");
+const express = require("express");
 
-var cat = require("../models/burger.js");
+const burger = require("../models/burger.js");
 
-var router = express.Router();
+const router = express.Router();
 
+// home route
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        let hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+// create new burger route
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "name"
+    ], [
+        req.body.name
+    ], function (result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
+});
