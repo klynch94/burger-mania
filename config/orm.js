0d025ee5bece63169523f9ebb89741dd1cf9b130
Kieran Lynch <lynch.kieran1@gmail.com>
2020-08-05 15:15:30 -0500

    created schema and seeds files, and flushed out ORM methods

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,90 @@
-let connection = require("../config/connection.js");
+const connection = require("../config/connection.js");
+
+// Code modified from a previous Cat Example
+// Helper function to create an array of question markets and turn them into a string..
+function printQuestionMarks(num) {
+    let arr = [];
+
+    for (let i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    let arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (let key in ob) {
+        let value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations 
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
+// ORM object with methods
+const orm = {
+    // select all function
+    all: function(tableInput, cb) {
+      const queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    // create function
+    create: function(table, cols, vals, cb) {
+      let queryString = "INSERT INTO " + table;
+  
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+  
+      console.log(queryString);
+  
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+    // update function
+    update: function(table, objColVals, condition, cb) {
+      let queryString = "UPDATE " + table;
+  
+      queryString += " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    }
+  };
+  
+  // Export the orm object for the model (cat.js).
+  module.exports = orm;
+  
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('BBQ Bacon Burger');
+INSERT INTO burgers (name) VALUES ('The Big Mac');
+INSERT INTO burgers (name) VALUES ('Bison Burger');
